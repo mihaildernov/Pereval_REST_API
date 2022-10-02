ba8b259c22dc79a176206eabf8f0d64212ab5fc1
dernov01@mail.ru <StalkerOnline1998>
2022-10-02 12:33:56 +0500

    ADD Models

--- Pereval_REST_API/fstp_pereval/models.py
+++ Pereval_REST_API/fstp_pereval/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from .config import Status
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=12)
+    name = models.CharField(max_length=25)
+    fam = models.CharField(max_length=25)
+    otc = models.CharField(max_length=25)
+
+
+class Pereval(models.Model):
+    beauty_title = models.CharField(max_length=10, default='пер.')
+    title = models.CharField(max_length=25)
+    other_titles = models.CharField(max_length=25)
+    connect = models.CharField(max_length=25, blank=True)
+    add_time = models.DateTimeField(auto_now_add=True)
+    level_winter = models.CharField(max_length=2, blank=True)
+    level_summer = models.CharField(max_length=2, blank=True)
+    level_autumn = models.CharField(max_length=2, blank=True)
+    level_spring = models.CharField(max_length=2, blank=True)
+    status = models.CharField(max_length=2, choices=Status, default='NW')
+
+
+class Coordinates(models.Model):
+    pereval = models.ForeignKey(Pereval, on_delete=models.CASCADE, related_name='coords')
+    latitude = models.CharField(max_length=15)
+    longitude = models.CharField(max_length=15)
+    height = models.CharField(max_length=15)
+
+
+class Image(models.Model):
+    data = models.ImageField()
+    title = models.CharField(max_length=50)
+
+
+class PerevalImage(models.Model):
+    photo = models.ForeignKey(Image, on_delete=models.CASCADE, related_name='image')
+    pereval = models.ForeignKey(Pereval, on_delete=models.CASCADE, related_name='pereval')
