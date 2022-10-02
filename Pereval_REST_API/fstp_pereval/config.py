c5f8bfbc5a83136c56932b26d5568a34a2a6e5f0
dernov01@mail.ru <StalkerOnline1998>
2022-10-02 12:32:31 +0500

    ADD Status

---
+++ Pereval_REST_API/fstp_pereval/config.py
@@ -0,0 +1,11 @@
+new = 'NW'
+pending = 'PD'
+accepted = 'AC'
+rejected = 'RJ'
+
+Status = {
+    (new, 'новый'),
+    (pending, 'принято на модерацию'),
+    (accepted, 'модерация прошла успешно'),
+    (rejected, 'модерация прошла, информация не принята')
+}
